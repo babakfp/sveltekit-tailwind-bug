ae4e436b9e90f2771f1a6e34493d21ea6bc14c29
babakfp <babak.bxf@gmail.com>
2022-01-05 01:30:35 -0800

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Comp from '$lib/Comp.svelte'
+</script>
+
+<div class="content">CONTENT</div>
+
+<div>
+	<Comp class="my-content">MY CONTENT</Comp>
+</div>
+
+<style lang="postcss">
+	div :global(.my-content) {
+		@apply flex items-center justify-center w-full h-20 border-2 border-red-500 border-dashed uppercase;
+	}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {},
+  plugins: [],
+}
+
+module.exports = config
